2a9c1a44787d4e713bf6cbcba4a8d85d83b60fb8
Guillermo Garcia Rebolo <guillermogarcia_18@hotmail.com>
2017-02-24 12:42:51 +0100

    Added framework

---
+++ Licensy/Licensy/Licensy.h
@@ -0,0 +1,19 @@
+//
+//  Licensy.h
+//  Licensy
+//
+//  Created by Guillermo Garcia Rebolo on 24/2/17.
+//  Copyright © 2017 RetoLabs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Licensy.
+FOUNDATION_EXPORT double LicensyVersionNumber;
+
+//! Project version string for Licensy.
+FOUNDATION_EXPORT const unsigned char LicensyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Licensy/PublicHeader.h>
+
+
