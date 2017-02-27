c8bcb488ee4fa38146aff270749b036eb7d4ea30
Guillermo Garcia Rebolo <guillermogarcia_18@hotmail.com>
2017-02-27 08:53:08 +0100

    Added example

--- Licensy/Licensy/Views/Appearance/Appearance.swift
+++ Licensy/Licensy/Views/Appearance/Appearance.swift
@@ -32,33 +32,33 @@ public class Appearance: NSObject {
 
     override init() {
         super.init()
-        setDefaultAppearance()
+        self.setDefaultAppearance()
     }
     
     fileprivate func setDefaultAppearance() {
-        headerContentColor = UIColor.black
-        headerBackgroundColor = UIColor.lightGray
-        accentColor = UIColor(red:0.00, green:0.48, blue:1.00, alpha:1.0)
-        roundLicenseButton = false;
+        self.headerContentColor = UIColor.black
+        self.headerBackgroundColor = UIColor.lightGray
+        self.accentColor = UIColor(red:0.00, green:0.48, blue:1.00, alpha:1.0)
+        self.roundLicenseButton = false;
     }
     
     /**
      Set a blue/green configuration appearance for the table
      */
     public func setBlueGreenAppearance() {
-        headerContentColor = UIColor.white
-        headerBackgroundColor = UIColor(red:0.15, green:0.30, blue:0.38, alpha:1.0)
-        accentColor = UIColor(red:0.00, green:0.50, blue:0.50, alpha:1.0)
-        roundLicenseButton = true;
+        self.headerContentColor = UIColor.white
+        self.headerBackgroundColor = UIColor(red:0.15, green:0.30, blue:0.38, alpha:1.0)
+        self.accentColor = UIColor(red:0.00, green:0.50, blue:0.50, alpha:1.0)
+        self.roundLicenseButton = true;
     }
     
     /**
      Set a light blue configuration appearance for the table
      */
     public func setLightBlueAppearance() {
-        headerContentColor = UIColor.white
-        headerBackgroundColor = UIColor(red:0.11, green:0.73, blue:0.84, alpha:1.0)
-        accentColor = UIColor(red:0.11, green:0.67, blue:0.80, alpha:1.0)
-        roundLicenseButton = false;
+        self.headerContentColor = UIColor.white
+        self.headerBackgroundColor = UIColor(red:0.11, green:0.73, blue:0.84, alpha:1.0)
+        self.accentColor = UIColor(red:0.11, green:0.67, blue:0.80, alpha:1.0)
+        self.roundLicenseButton = false;
     }
 }
