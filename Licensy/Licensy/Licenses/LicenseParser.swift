e59ecf92275e1226f34ec06fddcb19cafc783ba6
Willy GR <guillermogarcia_18@hotmail.com>
2018-03-27 22:44:15 +0200

    Refactored the license parser

--- Licensy/Licensy/Licenses/LicenseParser.swift
+++ Licensy/Licensy/Licenses/LicenseParser.swift
@@ -8,27 +8,32 @@
 
 import UIKit
 
-internal class LicenseParser: NSObject {
+internal class LicenseParser {
     
     fileprivate class var bundle: Bundle {
-        let podBundle =  Bundle(for: LibraryEntity.self)
+        let podBundle = Bundle(for: LibraryEntity.self)
         let bundleURL = podBundle.url(forResource: "Licensy", withExtension: "bundle")
         return Bundle(url: bundleURL!)!
     }
     
-    internal class func getContent(_ filename: String, inBundle bundle: Bundle = bundle) -> String! {
-        if let path = bundle.path(forResource: filename, ofType: "txt") {
-            let string: NSString?
-            do {
-                string = try NSString(contentsOfFile: path, encoding: String.Encoding.utf8.rawValue)
-                let stringValue = string as! String
-                return stringValue.replacingOccurrences(of: "\\n", with: "\n")
-            }
-            catch {
-                return nil
-            }
+    internal class func getContent(_ filename: String, inBundle bundle: Bundle = bundle) -> String? {
+        guard let path: String = bundle.path(forResource: filename, ofType: "txt") else {
+            return nil
         }
-        else {
+        guard let text: String = self.getText(from: path) else {
+            return nil
+        }
+        return text.replacingOccurrences(of: "\\n", with: "\n")
+    }
+    
+    //MARK: - Private method
+    
+    private class func getText(from path: String) -> String? {
+        do {
+            let value: String = try String(contentsOfFile: path, encoding: .utf8)
+            return value
+        }
+        catch {
             return nil
         }
     }
